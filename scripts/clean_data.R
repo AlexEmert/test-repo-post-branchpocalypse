d199c0f5e0ce1505e8d68383521e626bea4ffb6f
AlexEmert <alexemert@gmail.com>
2026-01-20 22:48:50 -0700

    add weather to data

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -6,4 +6,5 @@ glimpse(flights)
 
 flights_airline <- flights |>
   left_join(airlines, by = c("carrier" = "carrier")) |>
-  left_join(planes, by = c("tailnum" = "tailnum"))
+  left_join(planes, by = c("tailnum" = "tailnum")) |>
+  left_join(weather)
